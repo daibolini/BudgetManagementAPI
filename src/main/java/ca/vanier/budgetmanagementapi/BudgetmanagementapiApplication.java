9124e5dff0ab0f9fec89d588e5bfaac4cfa80f18
DESKTOP-PNMJLC1\uribe <daianabolini@hotmail.com>
2025-01-29 11:04:04 -0500

    initial commit

---
+++ src/main/java/ca/vanier/budgetmanagementapi/BudgetmanagementapiApplication.java
@@ -0,0 +1,13 @@
+package ca.vanier.budgetmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetmanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/vanier/budgetmanagementapi/BudgetmanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.vanier.budgetmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetmanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
