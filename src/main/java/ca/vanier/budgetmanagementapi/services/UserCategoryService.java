efa9cfd1f3631a59d522100f20a926db467c1b8d
DESKTOP-PNMJLC1\uribe <daianabolini@hotmail.com>
2025-01-30 18:15:37 -0500

    Added UserCategoryServiceImpl

--- src/main/java/ca/vanier/budgetmanagementapi/services/UserCategoryService.java
+++ src/main/java/ca/vanier/budgetmanagementapi/services/UserCategoryService.java
@@ -1,35 +1,9 @@
 package ca.vanier.budgetmanagementapi.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
 
-import ca.vanier.budgetmanagementapi.entity.Category;
-import ca.vanier.budgetmanagementapi.entity.Users;
-import ca.vanier.budgetmanagementapi.repository.CategoryRepository;
-import ca.vanier.budgetmanagementapi.repository.UserRepository;
+public interface UserCategoryService {
 
-public class UserCategoryService {
+    void addUserToCategory(Long userId, Long categoryId);
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private CategoryRepository categoryRepository;
-
-    public void addUserToCategory(Long userId, Long categoryId) {
-        Users user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new RuntimeException("Category not found"));
-        
-        if (!user.getCategories().contains(category)) {
-            user.getCategories().add(category);
-            userRepository.save(user);
-        }
-    }
-
-    public void removeUserFromCategory(Long userId, Long categoryId) {
-        Users user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
-        Category category = categoryRepository.findById(categoryId).orElseThrow(() -> new RuntimeException("Category not found"));
-
-        user.getCategories().remove(category);
-        userRepository.save(user);
-    }  
+    void removeUserFromCategory(Long userId, Long categoryId);
 }
--- src/main/java/ca/vanier/budgetmanagementapi/services/UserCategoryServiceImpl.java
+++ src/main/java/ca/vanier/budgetmanagementapi/services/UserCategoryServiceImpl.java
@@ -1,5 +1,42 @@
 package ca.vanier.budgetmanagementapi.services;
 
-public class UserCategoryServiceImpl {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ca.vanier.budgetmanagementapi.repository.UserRepository;
+import ca.vanier.budgetmanagementapi.entity.Category;
+import ca.vanier.budgetmanagementapi.entity.Users;
+import ca.vanier.budgetmanagementapi.repository.CategoryRepository;
+
+@Service
+public class UserCategoryServiceImpl implements UserCategoryService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Override
+    public void addUserToCategory(Long userId, Long categoryId) {
+        Users user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        if (!user.getCategories().contains(category)) {
+            user.getCategories().add(category);
+            userRepository.save(user);
+        }
+    }
+
+    @Override
+    public void removeUserFromCategory(Long userId, Long categoryId) {
+        Users user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        Category category = categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        user.getCategories().remove(category);
+        userRepository.save(user);
+    }
 }
